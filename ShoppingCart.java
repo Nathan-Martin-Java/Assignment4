607ee50c43fb9ce510388ac63ad6833cbb8c564a
MartinCzerwinski <martin.czerwinski@edu.sait.ca>
2016-11-22 11:06:09 -0700

    Populating Arrays

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,14 +1,49 @@
 
+import java.io.*;
+import java.util.*;
 
 public class ShoppingCart
 {
-	public static void main(String[] args)
+	static Scanner sc = new Scanner(System.in);
+	
+	
+	public static void main(String[] args) throws IOException
 	{
 		
 		
 		
+		String inFilename;
+		System.out.println("Please enter the name of the input file: ");
+		inFilename = sc.nextLine();
+		File file = new File(inFilename);
+		Scanner inputFile = new Scanner(file);
+		
+		int arraySize = getArraySize(inFilename);
+		String[] itemName = new String[arraySize];
+		double[] price = new double[arraySize];
+		int[] quantity = new int[arraySize];
+		//
+		for (int i=0; i<arraySize; i++)
+		{
+			String line=inputFile.nextLine();
+			String[] nextfield=line.split(",",-1);
+			
+			itemName[i] = nextfield[0];
+			price[i] = Double.parseDouble(nextfield[1]); 
+			quantity[i] = Integer.parseInt(nextfield[2]);
+		}
+		
+		int i = 0;
+		
+		while(i < arraySize)
+		{
+			System.out.println(itemName[i]);
+			System.out.println(price[i]);
+			System.out.println(quantity[i]);
+			System.out.println();
+			i++;
+		}
 	}
-	
 	public static int getArraySize(String inFilename) throws IOException
 	{
 		int i = 0;
