abe02288f305dd7ef86077c77d2cd4ba679cadd6
OnfireNFS <nathanf008@gmail.com>
2016-11-22 10:27:53 -0700

    Added Array Configure Thingy
    Added Array Configure Thingy. Actually its a method

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -8,4 +8,29 @@ public class ShoppingCart
 		
 		
 	}
+	
+	public static int getArraySize(String inFilename) throws IOException
+	{
+		int i = 0;
+		File file = new File(inFilename);
+		Scanner inputFile = new Scanner(file);
+		
+		String line=inputFile.nextLine();
+		
+		while(line!=null)
+		{
+			i++;
+			try
+			{
+				line=inputFile.nextLine();
+			}
+			
+			catch (Exception e)
+			{
+				break;
+			}
+			
+		}
+		return i;
+	}
 }
