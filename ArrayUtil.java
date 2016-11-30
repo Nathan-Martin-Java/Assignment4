0d3a49521226bd49bef866fa5b8cf6844618178c
OnfireNFS <nathanf008@gmail.com>
2016-11-30 14:43:55 -0700

    Commented ArrayUtil.Java and added broken
    Commented ArrayUtil.Java and added broken lists for error checking

--- ArrayUtil.java
+++ ArrayUtil.java
@@ -12,16 +12,29 @@ public class ArrayUtil
 {
 	public static int getIndex(String[] array, String search) 	//pass quantity and current price
 	{
-		int index = -1;
+		int index = -1;							//set index to -1 (for Error) if String was not found
+		Boolean wasFound = false;
 		
-		for(int i = 0; i < array.length; i++)
+		for(int i = 0; i < array.length; i++)	//for loop that runs while i is less than the number of elements in the array
 		{
-			if(search.equals(array[i]))
+			if(search.equals(array[i]) && wasFound == false)
+												//if the imported string "search" equals the array's string at index i and
+												//a string of that name has not been found yet
 			{
-				index = i;
+				index = i;						//index is equal to i because that is where the string
+				wasFound = true;					
+			}
+			
+			else if(search.equals(array[i]) && wasFound == true)	//if search = array at i and search has already been found
+			{														//print some error messages
+				System.out.printf("\n\nFATAL: Two Instances of %s Were Found Within The Array", search);
+				System.out.printf("\nFATAL: Check Your Infile For Duplicate Names");
+				System.out.printf("\nFATAL: Program Will Now Halt");
+				System.exit(-1);									//exit the program because it does not know how to differentiate
+																	//between the first and second instances of the found word and
+																	//the program does not know which instance to apply the discount to
 			}
 		}
-		
-		return index;
+		return index;	//return the index of the found string or return -1 if the string wasn't found
 	}
 }
